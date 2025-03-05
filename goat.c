4dd1b8d658f51953ae7e98cb049deaf287048083
Namadadi Iddi <150883718+Namadadi1@users.noreply.github.com>
2025-03-05 14:57:13 +0300

    Add files via upload

---
+++ goat.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("Namadadi is the goat");
+  return 0;
+}
---
+++ pass.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int mark = 60;
+    if (mark> 50){
+      printf("result : pass\n");
+      }
+      else{
+      printf("result : fail\n");
+      }
+      return 0;
+    }
